129abaf7b506099c69d1eccf6de0d9082c2ac5ed
ILYA <118203026+CHAMOMI1E@users.noreply.github.com>
2022-11-28 13:48:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class Air_playn(Transport_agency):
 i = 1
 dicts = {}
 while True:
-
+    cargo_weight = int(input("Введите вес груза в кг(1р/кг): "))
     distance = int(input("Введите расстояние для доставки (в км): "))
     large_of_city_output = int(input("Введите размер города отправителя (Большой-1\Средний-2\Маденький-3): "))
     large_of_city_input = int(input("Введите размер города получателя (Большой-1\Средний-2\Маденький-3): "))
@@ -49,21 +49,18 @@ while True:
     # ТС - самолет
     if large_of_city_output == 1 and large_of_city_input == 1:
         print(f"Вид траспорта - САМОЛЕТ - цена 3р/км ")
-        cargo_weight = int(input("Введите вес груза в кг(1р/кг): "))
         summ_for_transport = distance * 3 + cargo_weight  # сумма за всю перевозку
         print(f"ИТОГО: {summ_for_transport}")
         transport = Air_playn(distance)
         transport.set_earnings(summ_for_transport)
         print("Спасибо что выбрали нас!!!")
         # dicts.setdefault(i, f"из {large_of_city_output} города в {large_of_city_input} город, расстояние-{distance}, вес груза-{cargo_weight}")
-        dicts[
-            i] = f"из {large_of_city_output} города в {large_of_city_input} город, расстояние-{distance}, вес груза-{cargo_weight}"
+        dicts[i] = f"из {large_of_city_output} города в {large_of_city_input} город, расстояние-{distance}, вес груза-{cargo_weight}"
 
 
     # ТС - поезд
     elif large_of_city_output != 3 and large_of_city_input != 3 and large_of_city_input == 2 or large_of_city_output == 2:
         print(f"Вид траспорта - ПОЕЗД - цена 1р/км ")
-        cargo_weight = int(input("Введите вес груза в кг(1р/кг): "))
         summ_for_transport = distance + cargo_weight  # сумма за всю перевозку
         print(f"ИТОГО: {summ_for_transport}")
         transport = Railway(distance)
@@ -75,7 +72,6 @@ while True:
     # ТС - ААААВТОМОБИЛЬ!!!!
     elif large_of_city_output == 3 or large_of_city_input == 3:
         print(f"Вид траспорта - АВТОМОБИЛЬ - цена 2р/км ")
-        cargo_weight = int(input("Введите вес груза в кг(1р/кг): "))
         summ_for_transport = distance * 2 + cargo_weight  # сумма за всю перевозку
         print(f"ИТОГО: {summ_for_transport}")
         transport = Railway(distance)
